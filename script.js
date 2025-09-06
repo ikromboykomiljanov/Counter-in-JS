3493eeaae2ccd898eed830eb492b16f5cd985daa
ikromboy1 <ikromboykomiljanov761@gmail.com>
2025-09-06 07:43:42 +0500

    updated mini error

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const Counter = document.getElementById("Counter");
+const resultP = document.getElementById('resultP')
 let count = 0;
 
 function addCount() {
   count++;
   Counter.textContent = `Counter: ${count}`;
+  resultP.textContent = ""
+
 }
 
 function resetBtn() {
   Counter.textContent = `Counter: ${count = 0}`;
+  resultP.textContent = ""
+
 }
 function deleteBtn() {
   if (0 < count) {
